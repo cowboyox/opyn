f4ccea5735e9429f4f7abc30992e58a750271e73
Anton Cheng <antonassocareer@gmail.com>
2021-08-09 21:38:59 +0800

    fix: infura key inactive (#75)

--- src/components/Balancer/infura.js
+++ src/components/Balancer/infura.js
@@ -5,17 +5,14 @@ const controllerABI = require('./controllerabi.json');
 const oracleABI = require('./fakeOracleABI.json');
 const optionContractABI = require('../../constants/abi/OptionContract.json');
 
-const KEYS = [process.env.REACT_APP_INFURA_KEY, process.env.REACT_APP_INFURA_KEY2].filter((k) => k);
-const INFURA_KEY = KEYS[Math.floor(Math.random() * KEYS.length)];
+const INFURA_KEY = process.env.REACT_APP_INFURA_KEY;
 const web3 = new Web3(`https://kovan.infura.io/v3/${INFURA_KEY}`);
-
 
 const oToken = '0x6d2e52cac19d85fa1f8002c2b054502d962305e8';
 const usdc = '0x6de3919ad3e78a848a169c282d959de9a20267db';
 const controllerAddr = '0x8746BE1226DC25bdF70eD4Ff75ec3eB091749243';
 const poolAddr = '0x126679b2b632f643d00773b205e5a6b0559be282';
 const oracleAddr = '0xf53fea168194fd80214a8b8d876893b7aaada344';
-
 
 export const getIV = async () => {
   const controller = new web3.eth.Contract(controllerABI, controllerAddr);
